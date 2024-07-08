bd93aa874b0c905b1ab8faf01a2f4437259b178c
rameshwersamota <rameshwer.samota@gmail.com>
2024-07-08 17:15:50 +0530

    update version

--- example/versions.tf
+++ example/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">=1.7.4"
+  required_version = ">= 0.14, <= 1.9.1"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.50, < 5.0"
+      version = ">= 3.50, < 5.36.0"
     }
   }
 }
